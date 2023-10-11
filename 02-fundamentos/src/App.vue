2e95568c742f7612e360230c11dfa41ff31b9159
ojosdegatonegro <diegofernandocubides@gmail.com>
2023-10-10 21:24:23 -0500

    props, methods, computed props, components, validators, v-on, v-bind

---
+++ 02-fundamentos/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img src="./assets/logo.png" alt="Vue logo">
+  <Counter title="Entraron" v-bind:start="1+3"/>
+  <Counter :start="6" />
+  <Counter />
+</template>
+
+<script>
+
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
